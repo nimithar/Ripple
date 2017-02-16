6cf6334984c80b1276105730a0e15cf1d7760342
Nimitha Ramesh <nimitha.r.ramesh@gmail.com>
2017-02-16 09:02:04 -0800

    include comments

--- ripple_project/ripple_websocket.js
+++ ripple_project/ripple_websocket.js
@@ -1,13 +1,17 @@
 WebSocket = require('ws');
 
+// Create a WebSocket object that connects to the Ripple Server
 websocket = new WebSocket('wss://s1.ripple.com:443');
 
+// When connection is open
 websocket.on('open', () => {
-	console.log('Connceted to the Ripple Payment Network!')
+	console.log('Connceted to the Ripple Payment Network!');
 
+	// Subscribe to the network
 	websocket.send('{"command":"subscribe","id":0,"streams":["ledger"]}');
 });
 
+// Ledger Close events
 websocket.on('message', message => {
 	console.log('Message', message);
 });
